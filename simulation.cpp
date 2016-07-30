59fe223ada0b46b0cf8eb9216ae7c0dad5fbf0ea
xlog <efkaxlog@gmail.com>
2016-07-30 04:26:51 +0100

    particle data is now stored as multiple vectors containing seperate data, replacing one vector of particles

--- simulation.cpp
+++ simulation.cpp
@@ -11,50 +11,69 @@ void Simulation::togglePause() {
     simulationPaused = !simulationPaused;
 }
 
-void Simulation::makeCube() {
-    int index = 0;
-    float xPos, yPos, zPos;
-    xPos = yPos = zPos = 10.0f;
-    int side = 10;
-    float inc = xPos / side;
-    for(int i=0; i < side; i++) {
-        for(int j=0; j< side; j++) {
-            for(int k=0; k<side; k++) {
-                Particle particle(0.0001f, 0.0f, 0.0f, 0.0f, xPos, yPos, zPos, 0.1f, 1.0f, 1.0f, 1.0f);
-                particles.push_back(particle);
-                xPos -= inc;
-            }
-        xPos = 10.0f;
-        yPos -= inc;
-        }
-    yPos = 10.0f;
-    zPos -= inc;
-    }
-}
-
 void Simulation::generateRandomParticles(float range, bool canBeNegative) {
     int index = 0;
     while(index < particlesNumber) {
-        Particle particle = generateRandomParticle(range, canBeNegative);
-        particle.mass = 0.0001f;
-        particles.push_back(particle);
+        Particle p = generateRandomParticle(range, canBeNegative);
+        p.mass = 0.0001f;
+        addParticle(p.xPos, p.yPos, p.zPos, p.xForce, p.yForce, p.zForce,
+            p.mass, p.red, p.green, p.blue);
         index ++;
     }
 }
 
 void Simulation::calculateForces() {
-    for(Particle &particle : particles) {
-        for(Particle &otherParticle : particles) {
-            if(&particle == &otherParticle)  
+    for(int i=0; i<particlesNumber; i++) {
+        float particleX = xPositions[i];
+        float particleY = yPositions[i];
+        float particleZ = zPositions[i];
+        for(int j=0; j<particlesNumber;j++) {
+            if(i==j) {
                 continue;
-            float distance = physics->getDistance(&otherParticle, &particle);
-            particle.dx += physics->getAccX(&otherParticle, &particle, distance);
-            particle.dy += physics->getAccY(&otherParticle, &particle, distance);
-            particle.dz += physics->getAccZ(&otherParticle, &particle, distance);
+            }
+            float otherParticleX = xPositions[j];
+            float otherParticleY = yPositions[j];
+            float otherParticleZ = zPositions[j];
+            float otherParticleMass = masses[j];
+            float distance = physics->getDistance(
+                otherParticleX, particleX,
+                otherParticleY, particleY,
+                otherParticleZ, particleZ);
+            xForces[i] += physics->getForce(otherParticleX, particleX, otherParticleMass, distance);
+            yForces[i] += physics->getForce(otherParticleY, particleY, otherParticleMass, distance);
+            zForces[i] += physics->getForce(otherParticleZ, particleZ, otherParticleMass, distance);
         }
-        particle.xPos += particle.dx;
-        particle.yPos += particle.dy;
-        particle.zPos += particle.dz;
+        xPositions[i] += xForces[i];
+        yPositions[i] += yForces[i];
+        zPositions[i] += zForces[i];
     }
 }
 
+void Simulation::addParticle(
+        float xPos, float yPos, float zPos,
+        float xForce, float yForce, float zForce,
+        float mass, float red, float green, float blue) {
+    xPositions.push_back(xPos);
+    yPositions.push_back(yPos);
+    zPositions.push_back(zPos);
+    xForces.push_back(xForce);
+    yForces.push_back(yForce);
+    zForces.push_back(zForce);
+    masses.push_back(mass);
+    reds.push_back(red);
+    greens.push_back(green);
+    blues.push_back(blue);
+}
+
+Particle Simulation::getParticleAt(int i) {
+    return Particle(masses[i], xForces[i], yForces[i], zForces[i],
+        xPositions[i], yPositions[i], zPositions[i], reds[i], greens[i], blues[i]);
+}
+
+void Simulation::updatePositions() {
+    for (int i=0; i<particlesNumber; i++) {
+        xPositions[i] += xForces[i];
+        yPositions[i] += yForces[i];
+        zPositions[i] += zForces[i];
+    }
+}
--- simulation.h
+++ simulation.h
@@ -7,14 +7,28 @@
 class Simulation {
     public:
         int particlesNumber;
-        std::vector<Particle> particles;
+        std::vector<float> xPositions;
+        std::vector<float> yPositions;
+        std::vector<float> zPositions;
+        std::vector<float> xForces;
+        std::vector<float> yForces;
+        std::vector<float> zForces;
+        std::vector<float> masses;
+        std::vector<float> reds;
+        std::vector<float> greens;
+        std::vector<float> blues;
         bool simulationPaused;
         Physics *physics;
         Simulation(int particlesNumber, Physics *physics);
         void togglePause();
-        void makeCube();
         void generateRandomParticles(float range, bool canBeNegative);
         void calculateForces();
+        void addParticle(
+            float xPos, float yPos, float zPos,
+            float xForce, float yForce, float zForce,
+            float mass, float red, float green, float blue);
+        Particle getParticleAt(int index);
+        void updatePositions();
 };
 
 #endif /* SIMULATION_H */
